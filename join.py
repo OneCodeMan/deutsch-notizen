b680d46b0a9223c8f01b7f3167443b92124eeb7f
Dave Gumba <dave_gumba@yahoo.com>
2020-01-31 09:17:39 -0500

    pdf generates successfully

--- join.py
+++ join.py
@@ -31,16 +31,16 @@ all_lessons_html_file = 'all_lessons.html'
 all_lessons_pdf_file = 'german_lessons.pdf'
 
 all_markdown_content = ''
-lesson_files = [f for f in listdir() if f.endswith('.md')]
+lesson_files = [f for f in listdir('./lessons') if f.endswith('.md')]
 
 for lesson_file in lesson_files:
-    input_file = codecs.open(lesson_file, mode='r', encoding='utf-8')
+    input_file = codecs.open('./lessons/' + lesson_file, mode='r', encoding='utf-8')
     markdown_text = input_file.read()
     all_markdown_content += markdown(markdown_text)
 
 markdown_content_to_html_content = Markdown().convert(all_markdown_content)
 all_lessons_html_content = build_html_file(markdown_content_to_html_content)
-all_lessons_html = open('all_lessons.html', 'w', encoding='utf-8')
+all_lessons_html = open(all_lessons_html_file, 'w', encoding='utf-8')
 all_lessons_html.write(all_lessons_html_content)
 all_lessons_html.close()
 
